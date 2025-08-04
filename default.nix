445d97a73ada43beba1245c93faf257da635ebc4
Michele De Cillis <decillismicheledeveloper@gmail.com>
2025-08-04 15:50:13 +0200

    feat: changed from flake to default

---
+++ default.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.stdenv.mkDerivation {
+  name       = "BoligPortal.dk NotifyMe";
+  buildInputs = [
+    pkgs.python310
+    pkgs.python310Packages.requests
+    pkgs.python310Packages.beautifulsoup4
+    pkhs.python310Packages.python-dotenv
+  ];
+  src         = ./.;
+  installPhase = ''
+    mkdir -p $out/bin
+    cp script.py $out/bin/
+  '';
+}
